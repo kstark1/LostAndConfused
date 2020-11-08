ce76ac5599881e9357c53e20d1cece8e05fa5cd2
Eden <eknechte@ualberta.ca>
2020-11-08 00:57:57 -0700

    Cleaned up version, reorganized, removed print statements.

--- mainEdenVisual.py
+++ mainEdenVisual.py
@@ -10,11 +10,9 @@ SCREENDIM = (WIDTH, HEIGHT)
 
 # Colour Variables
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
 GREY = (50, 50, 50)
-BLUE = (0, 0, 255)
-DARK_GREEN = (28,84,35) # Used to fill in the rest of the screen that does not contain rects
-LIGHT_BLUE = (96, 166, 191) # Used for the walls (rects)
+DARK_GREEN = (28,84,35)
+LIGHT_BLUE = (96, 166, 191)
 ### Create the window
 
 screen = pygame.display.set_mode(SCREENDIM) # creates the main surface where all other assets are place on top
@@ -32,105 +30,6 @@ class Tile(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = (((x * 40) + 20), ((y * 30) + 15))
 
-class MazeGen():
-    def __init__(self):
-        self.maze = []
-        self.n = random.randrange(5, 8)
-        self.connections = []
-        self.row = 2
-        self.col = 0
-        self.dim = 20
-    
-    def empty_maze(self):
-        for i in range(0, self.dim):
-            self.maze.append([0]*self.dim)
-        
-    
-    def set_start(self):
-        self.connections.append([random.randrange(self.dim), 0])
-        self.row = self.connections[0][0]
-    
-    def set_end(self):
-        self.connections.append([random.randrange(self.dim), 19])
-    
-    def new_connection(self):
-        for i in range(0, self.n):
-            self.connections.append([random.randrange(0,self.dim), random.randrange(0, self.dim-1)])
-    
-    def connect_points(self):
-        self.maze[self.connections[0][0]][0] = 1
-        print(self.row, self.col)
-        for j in range(0, len(self.connections)):
-            # vertical movement
-            diff_row = self.connections[j][0] - self.row
-            if diff_row <= 0:
-                for i in range(0, diff_row*-1):
-                    self.row -= 1
-                    self.maze[self.row][self.col] = 1
-            if diff_row > 0:
-                for i in range(0, diff_row):
-                    self.row += 1
-                    self.maze[self.row][self.col] = 1
-                    
-            print(self.row, self.col)
-            # horizontal movement
-            print(j)
-            print("test", self.connections[j][1], self.col)
-            diff_col = self.connections[j][1] - self.col
-            if diff_col < 0:
-                for i in range(0, diff_col*-1):
-                    self.col -= 1
-                    self.maze[self.row][self.col] = 1
-            
-            if diff_col >=0:
-                for i in range(0, diff_col):
-                    self.col += 1
-                    self.maze[self.row][self.col] = 1
-                
-            print(self.row, self.col)
-            # horizontal movement
-
-    def print_maze(self):
-        # use for debugging
-        for row in self.maze:
-            print(row)
-
-    def get_maze(self):
-        return self.maze
-
-#Random tile to test collision
-#square = Tile()
-wall_tiles = pygame.sprite.Group()
-walk_tiles = pygame.sprite.Group()
-
-#if square.state == 0:
-#    wall_tiles.add(square)
-#else:
-#    walk_tiles.add(square)
-
-obj = MazeGen()
-obj.empty_maze()
-obj.set_start()
-obj.new_connection()
-obj.set_end()
-obj.connect_points()
-obj.print_maze()
-print(obj.connections)
-
-startingX = obj.connections[0][0] 
-
-positionState = obj.get_maze()
-for i in range(20):
-    for j in range(20):
-        if positionState[i][j] == 0:
-            colour = DARK_GREEN
-        else:
-            colour = LIGHT_BLUE
-        tile = Tile(j, i, positionState[i][j], colour)
-        if tile.state == 0:
-            wall_tiles.add(tile)
-        else:
-            walk_tiles.add(tile)
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, x):
@@ -144,6 +43,83 @@ class Player(pygame.sprite.Sprite):
         self.rect.x += x
         self.rect.y += y
 
+class MazeGen():
+    def __init__(self):
+        self.maze = []
+        self.n = random.randrange(5, 8)
+        self.connections = []
+        self.row = 2
+        self.col = 0
+        self.dim = 20
+    
+    def empty_maze(self):
+        for i in range(0, self.dim):
+            self.maze.append([0]*self.dim)
+        
+    def set_start(self):
+        self.connections.append([random.randrange(self.dim), 0])
+        self.row = self.connections[0][0]
+    
+    def set_end(self):
+        self.connections.append([random.randrange(self.dim), 19])
+    
+    def new_connection(self):
+        for i in range(0, self.n):
+            self.connections.append([random.randrange(0,self.dim), random.randrange(0, self.dim-1)])
+    
+    def connect_points(self):
+        self.maze[self.connections[0][0]][0] = 1
+        for j in range(0, len(self.connections)):
+            # vertical movement
+            diff_row = self.connections[j][0] - self.row
+            if diff_row <= 0:
+                for i in range(0, diff_row*-1):
+                    self.row -= 1
+                    self.maze[self.row][self.col] = 1
+            if diff_row > 0:
+                for i in range(0, diff_row):
+                    self.row += 1
+                    self.maze[self.row][self.col] = 1
+                    
+            diff_col = self.connections[j][1] - self.col
+            if diff_col < 0:
+                for i in range(0, diff_col*-1):
+                    self.col -= 1
+                    self.maze[self.row][self.col] = 1
+            
+            if diff_col >=0:
+                for i in range(0, diff_col):
+                    self.col += 1
+                    self.maze[self.row][self.col] = 1
+
+    def get_maze(self):
+        return self.maze
+
+obj = MazeGen()
+obj.empty_maze()
+obj.set_start()
+obj.new_connection()
+obj.set_end()
+obj.connect_points()
+
+wall_tiles = pygame.sprite.Group()
+walk_tiles = pygame.sprite.Group()
+
+positionState = obj.get_maze()
+
+for i in range(20):
+    for j in range(20):
+        if positionState[i][j] == 0:
+            colour = DARK_GREEN
+        else:
+            colour = LIGHT_BLUE
+        tile = Tile(j, i, positionState[i][j], colour)
+        if tile.state == 0:
+            wall_tiles.add(tile)
+        else:
+            walk_tiles.add(tile)
+
+startingX = obj.connections[0][0] 
 player_sprite = pygame.sprite.Group()
 player = Player(startingX)
 player_sprite.add(player)
@@ -176,9 +152,6 @@ while running:
                 if pygame.sprite.spritecollideany(player, wall_tiles, collided = None) != None:
                     player_sprite.update(0, -10)
 
-    # Testing out drawing the wall
-    #pygame.draw.rect(display, white, wall.draw_wall())	
-    
     pygame.display.update()
     screen.fill(GREY)
     
