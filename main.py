4d1886625317806ef55c9591048d3cc0b5eb859c
stansvan <47933204+stansvan@users.noreply.github.com>
2020-11-08 01:20:48 -0700

    yes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import random
+from mazegen import *
 
 ### Display Variables
 TITLE = 'Maze Game' # Title that appears in the window title
@@ -9,9 +11,9 @@ SCREENDIM = (WIDTH, HEIGHT)
 
 # Colour Variables
 WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
 GREY = (50, 50, 50)
-
+DARK_GREEN = (28,84,35)
+LIGHT_BLUE = (96, 166, 191)
 ### Create the window
 
 screen = pygame.display.set_mode(SCREENDIM) # creates the main surface where all other assets are place on top
@@ -20,16 +22,126 @@ screen.fill(GREY) # Fills the entire surface w/ the colour --> fill = erase/clea
 
 clock = pygame.time.Clock() # starts a clock obj to measure time
 
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, x, y, state, colour):
+        pygame.sprite.Sprite.__init__(self)
+        self.state = state #Can change to 1 to test over tile
+        self.image = pygame.Surface((40, 30))
+        self.image.fill(colour)
+        self.rect = self.image.get_rect()
+        self.rect.center = (((x * 40) + 20), ((y * 30) + 15))
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, x):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 20))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.center = (20, ((x * 30) + 15))
+
+    def update(self, x, y):
+        self.rect.x += x
+        self.rect.y += y
+
+    def get_grid_pos(self):
+        return [self.rect.y//30, self.rect.x//40]
+
+obj = MazeGen()
+obj.empty_maze()
+obj.set_start()
+obj.new_connection()
+obj.set_end()
+obj.connect_points()
+obj.print_maze()
+print(obj.connections)
+obj.set_dead_connects()
+obj.dead_end_points()
+obj.print_maze()
+
+wall_tiles = pygame.sprite.Group()
+walk_tiles = pygame.sprite.Group()
+
+positionState = obj.get_maze()
+
+for i in range(20):
+    for j in range(20):
+        if positionState[i][j] == 0:
+            colour = DARK_GREEN
+        else:
+            colour = LIGHT_BLUE
+        tile = Tile(j, i, positionState[i][j], colour)
+        if tile.state == 0:
+            wall_tiles.add(tile)
+        else:
+            walk_tiles.add(tile)
+
+startingX = obj.connections[0][0] 
+player_sprite = pygame.sprite.Group()
+player = Player(startingX)
+player_sprite.add(player)
+
 running = True
 
 while running:
-    for event in pygame.event.get(): # returns all inputs and triggers into an array
-        if event.type == pygame.QUIT: # if the red x was clicked eg buttons in corner of the window
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
 
-    clock.tick(FPS)  # pause the game until the FPS time is reached
-    pygame.display.flip()  # update the screen with changes, similar to sync() in AGK
+            if event.key == pygame.K_LEFT and (pygame.sprite.spritecollideany(player, wall_tiles, collided = None) == None):
+                player_sprite.update(-10, 0)
+                if pygame.sprite.spritecollideany(player, wall_tiles, collided = None) != None:
+                    player_sprite.update(10, 0)
 
-    pressedKeys = pygame.key.get_pressed()
+            if event.key == pygame.K_RIGHT and (pygame.sprite.spritecollideany(player, wall_tiles, collided = None) == None):
+                player_sprite.update(10, 0)
+                if pygame.sprite.spritecollideany(player, wall_tiles, collided = None) != None:
+                    player_sprite.update(-10, 0)
 
+            if event.key == pygame.K_UP and (pygame.sprite.spritecollideany(player, wall_tiles, collided = None) == None):
+                player_sprite.update(0, -10)
+                if pygame.sprite.spritecollideany(player, wall_tiles, collided = None) != None:
+                    player_sprite.update(0, 10)
+
+            if event.key == pygame.K_DOWN and (pygame.sprite.spritecollideany(player, wall_tiles, collided = None) == None):
+                player_sprite.update(0, 10)
+                if pygame.sprite.spritecollideany(player, wall_tiles, collided = None) != None:
+                    player_sprite.update(0, -10)
+
+            if event.key == pygame.K_r: ############################## regenerate maze
+                # regenerate maze
+                obj = MazeGen()
+                obj.empty_maze()
+                obj.set_start(player.get_grid_pos())
+                obj.new_connection()
+                obj.set_end()
+                obj.connect_points()
+                print(player.get_grid_pos())
+                obj.set_dead_connects()
+                obj.dead_end_points()
+                obj.print_maze()
+
+                walk_tiles.empty()
+                wall_tiles.empty()
+                positionState = obj.get_maze()
+                for i in range(20):
+                    for j in range(20):
+                        if positionState[i][j] == 0:
+                            colour = DARK_GREEN
+                        else:
+                            colour = LIGHT_BLUE
+                        tile = Tile(j, i, positionState[i][j], colour)
+                        if tile.state == 0:
+                            wall_tiles.add(tile)
+                        else:
+                            walk_tiles.add(tile)
+                            
+
+    pygame.display.update()
     screen.fill(GREY)
+    
+    wall_tiles.draw(screen)
+    walk_tiles.draw(screen)
+    
+    player_sprite.draw(screen)
