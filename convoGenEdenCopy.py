dd9a0d6027426dcd29b1f9bfb04b787ee580d5a4
Eden <eknechte@ualberta.ca>
2020-11-08 09:58:54 -0700

    Dictionary tiles spawn in walkable space, but can't increase number.

---
+++ convoGenEdenCopy.py
@@ -0,0 +1,60 @@
+import Conversation
+import random
+
+def generate_conversations(maze,screen):
+    """generates and asssociates locations of convos as a dictionary. 
+    keys are tuples of the location
+    returns said dictionary
+    all conversation writing should be done here!!"""
+
+    #generating random non-wall locations as tuples
+    random.shuffle(maze)
+    convoLocations = []
+    for x in range(15):
+            convoLocation = maze[x]
+            convoLocations.append(tuple(convoLocation))
+
+    
+    # adding each convo object paired with its location as a key
+    conversationDic = {}
+
+    """ create a new:
+
+            conversationDic[convoLocations[n]] = Conversation.Base_conversation
+            (nPrompt,option1,option2, correctAns, nPositiveResponse, nNegativeResponse,screen)
+
+        for each conversation we want added. ADD ONE TO N EACH TIME, AND MAKE SURE RANGE IN
+        LINE 15 IS THE TOTAL NUMBER OF CONVERSATIONS)"""
+
+    #convo 1
+    conversationDic[convoLocations[0]] = Conversation.Base_conversation ("nPrompt","option1","option2", "correctAns", "nPositiveResponse", "nNegativeResponse",screen)
+
+    #convo 2
+    conversationDic[convoLocations[1]] = Conversation.Base_conversation ("nPrompt","option1","option2", "correctAns", "nPositiveResponse", "nNegativeResponse",screen)
+
+    #convo 3
+    conversationDic[convoLocations[2]] = Conversation.Base_conversation ("nPrompt","option1","option2", "correctAns", "nPositiveResponse", "nNegativeResponse",screen)
+
+
+    return conversationDic
+
+def remove_convo(location,convoDictionary):
+    """remove a conversation from the dictionary after it has been used"""
+    convoDictionary.pop(location)
+
+def rerandomize_convos(convoDictionary,maze):
+    """ randomize convo placement again after having regenerated the maze"""
+    random.shuffle(maze)
+    convoLocations = []
+    for x in range(15):
+            convoLocation = maze[x]
+            print(maze[x])
+            convoLocations.append(tuple(convoLocation))
+    
+    """take each convo object from the original convo dictionary and re assign it
+    to  a new valid location from convolocations, then return a convo dictionary"""
+    locationListIterator = 0
+    newConvoDictionary ={}
+    for location in convoDictionary:
+        newConvoDictionary[convoLocations[locationListIterator]] = convoDictionary[location]
+    return newConvoDictionary
